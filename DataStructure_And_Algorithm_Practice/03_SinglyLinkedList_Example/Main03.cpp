2cf22e1de434429e5c6056cb71a396fc019dd395
wonhs717 <wonhs717@gmail.com>
2018-07-15 21:57:40 +0900

    Renaming main cpp files

---
+++ DataStructure_And_Algorithm_Practice/03_SinglyLinkedList_Example/Main03.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "SinglyLinkedList.h"
+
+int main()
+{
+	SinglyLinkedList *list = new SinglyLinkedList(true);
+
+	// List is empty
+	list->Remove();
+
+	list->Insert(3);
+	list->Insert(2);
+	list->Insert(1);
+	list->Insert(4);
+	list->Insert(5);
+
+	list->TestAllList();
+
+	// List is overflow
+	list->Insert(4);
+
+	list->TestAllList();
+
+	list->Remove(2);
+
+	list->TestAllList();
+
+	list->Remove();
+
+	list->TestAllList();
+
+	return 0;
+}
---
+++ DataStructure_And_Algorithm_Practice/04_CircularLinkedList_Example/Main04.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "CircularLinkedList.h"
+
+int main()
+{
+	return 0;
+}
