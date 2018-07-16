0b5c1b2db49eebbfe99f39eca65d1fd47150b6ce
wonhs717 <wonhs717@gmail.com>
2018-07-17 05:42:51 +0900

    Edit singly linked list no dummy

--- DataStructure_And_Algorithm_Practice/03_SinglyLinkedList_NoDummy_Example/SinglyLinkedListNoDummy.cpp
+++ DataStructure_And_Algorithm_Practice/03_SinglyLinkedList_NoDummy_Example/SinglyLinkedListNoDummy.cpp
@@ -1,12 +1,21 @@
 #include "SinglyLinkedListNoDummy.h"
 
-
-
 SinglyLinkedListNoDummy::SinglyLinkedListNoDummy()
 {
 }
 
-
 SinglyLinkedListNoDummy::~SinglyLinkedListNoDummy()
 {
 }
+
+bool SinglyLinkedListNoDummy::Insert(int data, int pos)
+{
+
+}
+
+bool SinglyLinkedListNoDummy::Remove(int pos)
+{
+
+}
+
+void SinglyLinkedListNoDummy::TestAllList();
--- DataStructure_And_Algorithm_Practice/03_SinglyLinkedList_NoDummy_Example/SinglyLinkedListNoDummy.h
+++ DataStructure_And_Algorithm_Practice/03_SinglyLinkedList_NoDummy_Example/SinglyLinkedListNoDummy.h
@@ -1,8 +1,24 @@
 #pragma once
+
+struct Node
+{
+	int data;
+	Node *next;
+};
+
 class SinglyLinkedListNoDummy
 {
 public:
 	SinglyLinkedListNoDummy();
 	~SinglyLinkedListNoDummy();
+
+	bool	Insert(int data, int pos = 0);
+	bool	Remove(int pos = 0);
+	void	TestAllList();
+
+private:
+	Node	*head;
+
+	int		nodeCnt;
 };
 
