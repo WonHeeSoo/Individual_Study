4b901a1693dcede864d552fde0b8e28fe13f150e
wonhs717 <wonhs717@gmail.com>
2018-07-20 21:12:50 +0900

    Edit circular linked list

--- DataStructure_And_Algorithm_Practice/04_CircularLinkedList_Example/CircularLinkedList.cpp
+++ DataStructure_And_Algorithm_Practice/04_CircularLinkedList_Example/CircularLinkedList.cpp
@@ -1,11 +1,18 @@
 #include "CircularLinkedList.h"
 
-CircularLinkedList::CircularLinkedList() : nodeCnt(0)
+CircularLinkedList::CircularLinkedList() : tail(0), nodeCnt(0)
 {
 }
 
 CircularLinkedList::~CircularLinkedList()
 {
+	Node *cur = tail.next;
+	Node *del = cur;
+	for (int i = 0; i < nodeCnt; i++)
+	{
+		delete del;
+		cur = cur->next;
+	}
 }
 
 bool CircularLinkedList::Insert(int data, int pos)
@@ -19,19 +26,72 @@ bool CircularLinkedList::Insert(int data, int pos)
 	{
 		Node *newNode = new Node(data);
 		tail.next = newNode;
+		newNode->next = newNode;
 		nodeCnt++;
 		return true;
 	}
 
+	if (nodeCnt !=0 && nodeCnt == pos)
+	{
+		Node *newNode = new Node(data);
+		newNode->next = tail.next;
+		tail.next = newNode;
+		tail = *newNode;
+		nodeCnt++;
+		return true;
+	}
 
+	Node *tailNode = &tail;
+	Node *nextNode = tail.next;
+	for (int i = 0; i < __min(pos, nodeCnt); i++)
+	{
+		tailNode = tailNode->next;
+		nextNode = nextNode->next;
+	}
+	Node *newNode = new Node(data);
+	tailNode->next = newNode;
+	newNode->next = nextNode;
+	nodeCnt++;
+	return true;
 }
 
 bool CircularLinkedList::Remove(int pos)
 {
+	if (pos < 0 || nodeCnt == 0)
+	{
+		return false;
+	}
 
+	if (nodeCnt == 1)
+	{
+		
+		delete tail.next;
+		nodeCnt--;
+		return true;
+	}
+
+	Node *prev = &tail;
+	Node *del = tail.next;
+	Node *next = del->next;
+	for (int i = 0; i < __min(pos, nodeCnt-1); i++)
+	{
+		prev = prev->next;
+		del = del->next;
+		next = next->next;
+	}
+	prev->next = next;
+	delete del;
+	nodeCnt--;
+	return true;
 }
 
 void CircularLinkedList::TestAllList()
 {
-
+	Node *cur = tail.next;
+	for (int i = 0; i < nodeCnt; i++)
+	{
+		printf("%d ", cur->data);
+		cur = cur->next;
+	}
+	printf("\n---\n");
 }
--- DataStructure_And_Algorithm_Practice/04_CircularLinkedList_Example/Main04.cpp
+++ DataStructure_And_Algorithm_Practice/04_CircularLinkedList_Example/Main04.cpp
@@ -3,5 +3,28 @@
 
 int main()
 {
+	CircularLinkedList *list = new CircularLinkedList();
+
+	list->Insert(1);
+	
+	list->TestAllList();
+
+	list->Remove();
+
+	list->TestAllList();
+
+	list->Insert(1);
+	list->Insert(2);
+
+	list->TestAllList();
+
+	list->Insert(3,1);
+
+	list->TestAllList();
+
+	list->Remove(5);
+
+	list->TestAllList();
+
 	return 0;
 }
