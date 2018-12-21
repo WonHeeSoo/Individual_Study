d50b3f912df73352850f87f1a815953045e89e18
wonhs717 <wonhs717@gmail.com>
2018-12-21 13:44:36 +0900

    Edit MST Undirected KruskalAlgorithm

--- DataStructure_And_Algorithm_Practice/22_MST_Undirected_KruskalAlgorithm_Example/UndirectedGraphKruskal.cpp
+++ DataStructure_And_Algorithm_Practice/22_MST_Undirected_KruskalAlgorithm_Example/UndirectedGraphKruskal.cpp
@@ -1,6 +1,7 @@
 #include "UndirectedGraphKruskal.h"
 #include <iostream>
 #include <stack>
+#include <assert.h>
 
 const int Edge::INVALID_ID = -1;
 const int Vertex::INVALID_ID = Edge::INVALID_ID;
@@ -62,37 +63,42 @@ bool UndirectedGraphKruskal::RemoveEdge(int aNode, int bNode, double weight)
 	if (allEdges[aNode].empty())
 		return false;
 
+	bool aFound = false;
 	const list<Edge> &aEdge = allEdges[aNode];
 	
 	list<Edge>::const_iterator aIt;
-	for (aIt = aEdge.begin(); aIt != aEdge.end();)
+	for (aIt = aEdge.begin(); aIt != aEdge.end(); aIt++)
 	{
-		if (aIt->endVertex == bNode || aIt->weight == weight)
+		if (aIt->endVertex == bNode && aIt->weight == weight)
 		{ // 도착하는 Vertex 위치가 같고, 무게가 같으면
-			allEdges[aNode].erase(aIt);
+			aFound = true;
 			break;
-		}
-		else
-		{
-			aIt++;
 		}
 	}
 
+	bool bFound = false;
 	const list<Edge> &bEdge = allEdges[bNode];
 
 	list<Edge>::const_iterator bIt;
-	for (bIt = bEdge.begin(); bIt != bEdge.end();)
+	for (bIt = bEdge.begin(); bIt != bEdge.end(); bIt++)
 	{
-		if (bIt->endVertex == aNode || bIt->weight == weight)
+		if (bIt->endVertex == aNode && bIt->weight == weight)
 		{ // 도착하는 Vertex 위치가 같고, 무게가 같으면
-			allEdges[bNode].erase(bIt);
-			nEdge--;
-			return true;
+			bFound = true;
+			break;
 		}
-		else
-		{
-			bIt++;
-		}
+	}
+
+	if (aFound && bFound)
+	{
+		allEdges[aNode].erase(aIt);
+		allEdges[bNode].erase(bIt);
+		nEdge--;
+		return true;
+	}
+	else if (aFound != bFound)
+	{
+		assert("ERROR : Inconsistent edge state.");
 	}
 
 	return false;
@@ -119,7 +125,7 @@ bool UndirectedGraphKruskal::IsConnected(int aNode, int bNode)
 		Vertex &curVertex = vertices[vertexStack.top()];
 		vertexStack.pop();
 
-		if (curVertex.id == vertices[bNode].id)
+		if (curVertex.id == bNode)
 			return true;
 
 
@@ -136,7 +142,6 @@ bool UndirectedGraphKruskal::IsConnected(int aNode, int bNode)
 		}
 	}
 
-	ResetVisited();
 	return false;
 }
 
@@ -144,7 +149,9 @@ bool UndirectedGraphKruskal::BuildSpanningTree()
 {
 	PrintAllEdges();
 
-	list<Edge> restoreEdges;
+	if ((nVertex - nEdge) > 1) // Vertex가 하나라도 연결이 되어있지 않다면
+		return false;
+
 	priority_queue<Edge> priorityEdges;
 	int vertexIndex = 0;
 
@@ -155,19 +162,15 @@ bool UndirectedGraphKruskal::BuildSpanningTree()
 		list<Edge>::const_iterator it;
 		for (it = edges.begin(); it != edges.end(); it++)
 		{
-			priorityEdges.push(*it);
+			if (it->startVertex < it->endVertex)
+				priorityEdges.push(*it);
 		}
 	}
 
 	while (!priorityEdges.empty())
 	{
-		if ((nVertex - nEdge) > 1) // Vertex가 하나라도 연결이 되어있지 않다면
-			return false;
-
 		// priorityEdges에서 가중치가 제일 높은 Edge의 정보를 꺼낸다
 		Edge heavyEdge = priorityEdges.top();
-		restoreEdges.push_back(heavyEdge);
-		priorityEdges.pop(); // Undirected
 		priorityEdges.pop();
 
 		// priorityEdges에서 꺼낸 Edge를 실제로 그래프에서 삭제한다
@@ -175,12 +178,18 @@ bool UndirectedGraphKruskal::BuildSpanningTree()
 
 		// Edge를 삭제하고 두 Vertex를 연결하는 경로가 있는지 확인한다
 		if (!IsConnected(heavyEdge.startVertex, heavyEdge.endVertex)) // 연결하지 못했다면
-		{
-			InsertEdge(restoreEdges.back().startVertex, restoreEdges.back().endVertex, restoreEdges.back().weight);
-			restoreEdges.pop_back();
-		}
+			InsertEdge(heavyEdge.startVertex, heavyEdge.endVertex, heavyEdge.weight);
 	}
 	PrintAllEdges();
+
+	for (int i = 0; i < nVertex - 1; i++)
+	{
+		for (int j = i + 1; j < nVertex; j++)
+		{
+			if (!IsConnected(i, j))
+				return false;
+		}
+	}
 
 	return true;
 }
--- DataStructure_And_Algorithm_Practice/22_MST_Undirected_KruskalAlgorithm_Example/main22.cpp
+++ DataStructure_And_Algorithm_Practice/22_MST_Undirected_KruskalAlgorithm_Example/main22.cpp
@@ -1,20 +1,24 @@
 #include "UndirectedGraphKruskal.h"
+#include <iostream>
 
 int main()
 {
 
 	UndirectedGraphKruskal* graph = new UndirectedGraphKruskal(5);
 	graph->InsertEdge(0, 1, 4.);
-	graph->InsertEdge(0, 2, 2.);
+	graph->InsertEdge(0, 2, 3.);
 	graph->InsertEdge(0, 4, 3.);
 	graph->InsertEdge(1, 4, 5.);
 	graph->InsertEdge(1, 3, 1.);
 	graph->InsertEdge(2, 4, 6.);
-	graph->InsertEdge(3, 0, 8.);
+	graph->InsertEdge(3, 0, 1.);
 	graph->InsertEdge(3, 4, 9.);
-	graph->InsertEdge(4, 0, 7.);
+	graph->InsertEdge(4, 0, 6.);
 
-	graph->BuildSpanningTree();
+	if (graph->BuildSpanningTree())
+		cout << "Success MST" << endl;
+	else
+		cout << "Fail" << endl;
 
 	return 0;
 }
